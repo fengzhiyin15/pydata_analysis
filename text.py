b9016e0f3f98b14461804eeacc47c59d3c52b998
fengzhiyin15 <34828635+fengzhiyin15@users.noreply.github.com>
2018-03-17 21:31:05 +0800

    Add files via upload

---
+++ text.py
@@ -0,0 +1,13 @@
+df=pd.DataFrame()
+for i in range(30):
+    df[cancer['feature_names'][i]]=cancer.data[:,i]
+df.T.head()
+
+    rc=zip(range(569),cancer.data[:,i])
+    df.loc[cancer['feature_names'][i]=rc
+df.head()
+
+df.append( )
+
+
+r[-1, ::2]  This is a slice of the last row, and only every other element.
